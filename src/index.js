0ea8f657758c35ca77c86619bb1758ebd1e0680a
Rodrigo-Pinatti <rodrigo.gomez.pinatti@gmail.com>
2024-06-19 19:10:09 -0300

    Se agregó comentarios a cada linea del codgio

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
+//Se cargan variables de entorno desde un archivo .env
 require('dotenv').config();
+
+//Se importa el modulo express
 const express = require('express');
+
+//Se instancia una aplicacion
 const app = express();
+
+//Se importa el modulo path que sirve para manejar rutas de archivo
 const path = require('path');
 
-
+//Configura la aplicación para que pueda parsear los datos de formularios enviados con application/urlencoded. 
+//extended: false indica que solo se pueden parsear objetos de tipo string o array.
+//app.use(express.json());: Configura la aplicación para que pueda parsear cuerpos de solicitudes en formato JSON.
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+//Incluye y usa el enrutador definido en (./routes/index.js.) Esto permite organizar las rutas de la aplicación en archivos separados.
 app.use(require('./routes/index.js'));
 
-
+//Configura un middleware para servir archivos estáticos desde el directorio public. 
+//path.join(__dirname, 'public') construye la ruta absoluta al directorio public.
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+//Define el puerto en el que se ejecutará el servidor. Si process.env.PORT está definido, se usa ese valor; de lo contrario, se usa el puerto 3000.
+//Se inicia el servidor y lo hace escuchar en el puerto 3000. Cuando el servidor está listo, se ejecuta el callback que imprime "Server en puerto 3000" en la consola.
 const PORT = process.env.PORT || 3000;
 app.listen(3000, ()=>{
     console.log('Server en puerto 3000');
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,27 @@
-//se importan las librerias
+// Carga las variables de entorno desde un archivo .env al process.env
 require('dotenv').config();
+
+// Importa el módulo Router de express
 const { Router } =require('express');
+
+// Importa nodemailer para enviar correos electrónicos
 const nodemailer=require('nodemailer');
+
+// Crea una nueva instancia de un router de Express
 const router = Router();
 
-
+// Define la ruta para manejar solicitudes POST en /send-email
 router.post('/send-email', async (req, res) => {
+     // Extrae los datos del cuerpo de la solicitud
     const { name, email, telefono, fecha, turno } = req.body;
 
 
-
+    // Configura el transporte de correo utilizando nodemailer
     const transporter = nodemailer.createTransport({
+        // Dirección del servidor y puerto SMTP, obtenida de las variables de entorno
         host: process.env.SMTP_HOST,
         port: process.env.SMTP_PORT,
-        secure: false,
-        //service:'gmail',
+        secure: false,// Indica si se debe usar una conexión segura (false para conexiones no seguras)
         auth:{
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS
@@ -24,20 +31,24 @@ router.post('/send-email', async (req, res) => {
     });
 
     try{
+        // Envía el correo electrónico
         const info = await transporter.sendMail({
             from: "ConectarLAB",
+            // Destinatario del correo, variable que se obtiene del formulario
             to: email,
             subject:"Reserva de turno",
             text:`${name}, gracias por realizar su reserva. Recuerde que el día ${fecha}, turno ${turno}, tiene su reserva.`, 
         });
-
+        // Muestra un mensaje en consola indicando que el correo fue enviado exitosamente
         console.log('Correo enviado exitosamente', info.messageId);
-
+         // Redirige al usuario a la página de éxito
         res.redirect('/success.html');
     }catch (error){
+         // Muestra un mensaje de error en consola si ocurre un problema al enviar el correo
         console.error('Error al enviar el correo:', error);
+        // Envía una respuesta de error al cliente
         res.status(500).send('Error al enviar el correo');
     }
 });
-
+// Exporta el router para que pueda ser utilizado en otros archivos
 module.exports = router;
